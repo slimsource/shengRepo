531e6db75b7bf40ef8236cd8b97c11203c2699a9
slimsource <radiocontrol1010@gmail.com>
2019-02-10 15:35:28 -0800

    First commit

---
+++ Small/tryRegEx/matchFirstLastWord.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -------------------------------------------------#
+# Match first and last word and say True or False
+# Here we try to see the sentence is that It starts
+# with the word "The", then bla, bla, bla, then ends
+# with the word "Spain".
+# --------------------------------------------------#
+import re
+
+txt = "The rain in Spain"
+
+x = re.search("^The.*Spain$", txt)  # This line returns TRUE or FALSE
+
+if x:
+	print("Yes, We have a match!")
+else:
+	print("No match!")
+
+
+
+
+
