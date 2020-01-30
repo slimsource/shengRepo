d25808976ddc26d748faf764c1278220c4b0fc05
slimsource <radiocontrol1010@gmail.com>
2020-01-29 18:02:51 -0800

    Initial commit basicPlot.py

---
+++ Python/Basic/Plot/basicPlot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#-----------------------------
+#
+# To plot a 2-dimensional plot
+# 
+#-----------------------------
+import matplotlib.pyplot as plt 
+
+
+plt.plot([1,2,3,2,1],[1,2,3,2,1]) #1st group is x-axis, 2nd is y-axis
+
+plt.show()
+
+
+
