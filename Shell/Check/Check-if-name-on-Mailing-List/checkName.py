eb8a312fb6b904691bbd5d21b25927a1f5e959b4
slimsource <radiocontrol1010@gmail.com>
2020-01-31 18:57:24 -0500

    Add tutorial in comment area

--- Shell/Check/Check-if-name-on-Mailing-List/checkName.py
+++ Shell/Check/Check-if-name-on-Mailing-List/checkName.py
@@ -1,9 +1,13 @@
 #!/bin/bash
-#----------------------------------------------------
+#--------------------------------------------------------------------
 # Enter a name and show if it is on the list or not
 # Syntax: ./<script> <name>
-#----------------------------------------------------
-grep $1 mailingList.txt > /dev/null #Do Not show the output of grep, redirect to /dev/null
+#
+# if loop
+# $?            : based on last user entry, return 0 if pass, 1 for fail 
+# > /dev/null   : Hide output of command
+#--------------------------------------------------------------------
+grep $1 mailingList.txt > /dev/null # hide output
 
 if [ $? -ne 0 ]; then
 echo $1 'is NOT on the mailing list, Sorry!'
